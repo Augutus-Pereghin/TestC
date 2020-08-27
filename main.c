beb2b7d838174e8bd8adb5b3fd3b7c6727680fd5
AugutusPereghin <agp5191@psu.edu>
2020-08-27 02:45:44 +0000

    added input for name

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <readline/readline.h>
 
 int main(void) {
   printf("Hello World\n");
+  char* name = readline("enter your name:");
+  printf("Hello, %s!" , name);
   return 0;
 }
